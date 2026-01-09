14a014c78565f0ede5bd09d33b252c93f11ee414
Magdum <pirjademl7@gmail.com>
2026-01-09 20:30:47 +0530

    wip : keep url as of now

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiInstance = axios.create({
-  baseURL: "http:localhost:8000/api/v1",
+  baseURL: "http://localhost:8000/api/v1",
   timeout: 1000,
   headers: {
     "Content-Type": "application/json",
